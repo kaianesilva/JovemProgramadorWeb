d0bda1ab7e5ee6aa1b3983edeb2a81c24268e82b
kaianesilva <kaiane1524@gmail.com>
2023-07-26 21:31:31 -0300

    permissões

--- JovemProgramadorWeb/Controllers/HomeController.cs
+++ JovemProgramadorWeb/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace JovemProgramadorWeb.Controllers
             return View();
         }
 
+        public IActionResult Alterações()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
